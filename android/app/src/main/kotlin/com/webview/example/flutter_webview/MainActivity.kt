b3e16f29e41285b442ec178bf78f6e17cdde83d9
NasarAli <nasarsyed.6000@gmail.com>
2024-01-27 10:30:58 +0530

    first commit

---
+++ android/app/src/main/kotlin/com/webview/example/flutter_webview/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.webview.example.flutter_webview
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/homepage.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_webview/webview_container.dart';
+
+class HomePage extends StatelessWidget {
+  HomePage({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: SafeArea(
+            child: SingleChildScrollView(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    Container(
+                        padding: const EdgeInsets.all(20.0),
+                        child: TextButton(
+                          style: TextButton.styleFrom(
+                            padding:  const EdgeInsets.symmetric(horizontal: 50.0, vertical: 15.0),
+                            backgroundColor: Colors.blue,
+                            textStyle: const TextStyle(fontSize: 30,fontWeight: FontWeight.bold),
+                          ),
+                          child: Text('Open Link'),
+                          onPressed: () => _handleURLButtonPress(context),
+                        ))
+                  ],
+                ))));
+  }
+
+  void _handleURLButtonPress(BuildContext context) {
+    Navigator.push(context,
+        MaterialPageRoute(builder: (context) => WebViewContainer()));
+  }
+}
